6619b3e2e040fa7f496573a057a09f22cf95ca7b
habibaa74 <habibaahmeda242@gmail.com>
2025-03-12 23:52:38 +0200

    sec commitm

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,15 +5,15 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   return (
     
-    <div className='flex'>
-      <div className="w-1/2 relative">
-      <h1 className='block text-7xl text-white font-bold pl-2 absolute top-[30%]'>Enjoy Our
+    <div className='flex justify-between items-center gap-2'>
+      <div className="w-1/2">
+      <h1 className='block text-4xl md:text-7xl text-white font-bold pl-2 sm:w-full'>Enjoy Our
       Delicious Recipes</h1>
-      <Link to="meals"><button className='bg-[orangered] text-white py-2 px-7 absolute top-[65%] left-5 font-semibold font-[Kanit] text-2xl cursor-pointer'>Recipes <i className="fa-solid fa-arrow-right"></i></button>
+      <Link to="meals"><button className='block bg-[orangered] text-white py-2 px-7 font-semibold font-[Kanit] text-2xl cursor-pointer mt-7'>Recipes <i className="fa-solid fa-arrow-right"></i></button>
 </Link>
       </div>
       <div className="w-1/2 overflow-hidden">
-      <img src={hero} className={styles.img} alt="hero image" />
+      <img src={hero} className={`${styles.img} w-full`} alt="hero image" />
       </div>
     </div>
   )
